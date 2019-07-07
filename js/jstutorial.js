8ce99c10ca8dca68da1dd8c8ba84accf93a9dd7d
Ernesto Ruiz <bc2rugue@gmail.com>
2019-07-07 16:36:54 +0200

    Update jstutorial.js
    added quotes

--- js/jstutorial.js
+++ js/jstutorial.js
@@ -3,7 +3,7 @@
  */
 
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!;
+myHeading.textContent = 'Hello world!';
 
 var myImage = document.querySelector('img');
 
